c1822b008f65b8ea0a9d0431d4cd13eb7fb4343a
Albert Salikhov <bert56@yandex.ru>
2024-03-19 16:38:48 +0300

    fix

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 class Category:
+    total_number_of_categories = 0
+    total_number_of_unique_products = 0
+
     def __init__(self, name:str, description:str, products:list):
         self.name = name # название
         self.descriptions = description # описание
-        self.categories = []  # общее количество категорий
         self.__products = products  # список продуктов
+        Category.total_number_of_categories += 1  # общее количество категорий
+        Category.total_number_of_unique_products += len(self.__products)
 
-    def product_list(self, product):
-        self.__products.append(product)
-        return self.__products
+    def add_products(self, *args):
+        self.__products.append(args)
+
 
     @property
-    def products(self):
+    def print_products(self):
         products_str = []
         for product in self.__products:
-            products_str.append(f'{product.name}, {product.price} руб. Остаток: {product.quantity_stock} шт.')
+            products_str.append( f'{product.name}, {product.price} руб. Остаток: {product.quantity_stock} шт.')
         return products_str
 
 
@@ -27,18 +31,18 @@ class Product:
 
 
     @classmethod
-    def products(cls, obj):
+    def products(cls, **obj):
         name, price, quantity_stock = obj
         return cls(name, price, quantity_stock)
 
     @property
     def get_price(self):
-        if self.price <=0:
-            print('Цена введена некорректная')
         return self.price
 
     @get_price.setter
     def get_price(self, price):
+        if self.price <=0:
+            print('Цена введена некорректная')
         self.price = price
 
 
@@ -46,9 +50,10 @@ class Product:
 
 
 if __name__ == '__main__':
-    product1 = Product('ноутбук', 'описание', '25000.00', 15)
-    product2 = Product('телевизор', 'описание', '50000.00', 10)
+    product1 = Product('ноутбук', 'описание', 25000.00, 15)
+    product2 = Product('телевизор', 'описание', 50000.00, 10)
     list_products = [product1, product2]
-    category = Category('электроника', 'описание', list_products)
-    category.product_list(list_products)
-    print(Category.products)
+    category1 = Category('электроника', 'описание', list_products)
+    print(category1.print_products)
+    print(Category.total_number_of_categories)
+
