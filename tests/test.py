cc1602cff1ffd2fee9834fbdccf681270a8602dc
Albert Salikhov <bert56@yandex.ru>
2024-03-23 02:06:55 +0300

    test add

--- tests/test.py
+++ tests/test.py
@@ -14,12 +14,25 @@ def test_category(category):
     assert category.total_number_of_unique_products == 6
 
 @pytest.fixture()
-def prod():
+def prod1():
     return Product('ноутбук', 'описание', 25000.00, 15)
 
-def test_prod(prod):
-    assert prod.name == 'ноутбук'
-    assert prod.descriptions == 'описание'
-    assert prod.price == 25000.00
-    assert prod.quantity_stock == 15
+@pytest.fixture()
+def prod2():
+    return Product('телефон', 'описание', 37000.00, 27)
+
+def test_prod(prod1):
+    assert prod1.name == 'ноутбук'
+    assert prod1.descriptions == 'описание'
+    assert prod1.price == 25000.00
+    assert prod1.quantity_stock == 15
+
+def test_prod_2(prod2):
+    assert prod2.name == 'телефон'
+    assert prod2.descriptions == 'описание'
+    assert prod2.price == 37000.00
+    assert prod2.quantity_stock == 27
+
+def test_add(prod1, prod2):
+    assert prod1 + prod2 == 42
 
